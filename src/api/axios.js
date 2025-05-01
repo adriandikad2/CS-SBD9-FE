e4254a825afc26e31b19747633c4a4e227554c30
adriandikad2 <adriandikad2@gmail.com>
2025-05-02 01:02:12 +0700

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "/api", // Fallback to /api for local development
+  baseURL: import.meta.env.VITE_API_URL || "/api", // Fallback to /api for local development
   headers: {
     "Content-Type": "application/json",
   },
